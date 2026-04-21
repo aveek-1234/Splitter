9eb7ba44c3d998be03a9cdd184c0683239d38ead
aveek-1234 <aveekkarmakar28@gmail.com>
2026-04-21 20:19:22 +0530

    minor fixes regarding convex config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,10 @@
-export default{
-    providers:[
+const authConfig = {
+    providers: [
         {
-            domain:process.env.CLERK_JWT_ISSUER_DOMAIN,
-            applicationID:"convex"
+            domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+            applicationID: "convex"
         },
     ],
 };
+
+export default authConfig;
