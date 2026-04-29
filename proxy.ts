0ebafb6c2ab27c66d56b789e995bb1354afcf5cc
aveek-1234 <aveekkarmakar28@gmail.com>
2026-04-29 14:42:25 +0530

    fixed proxy

--- proxy.ts
+++ proxy.ts
@@ -1,36 +1,27 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/settings(.*)',
-  '/contacts(.*)',
-  '/groups(.*)',
-  '/person(.*)',
-  '/settlements(.*)',
+  "/dashboard(.*)",
+  "/settings(.*)",
+  "/contacts(.*)",
+  "/groups(.*)",
+  "/person(.*)",
+  "/settlements(.*)",
 ]);
 
-const allowedRoutes = createRouteMatcher([
-  '/api/inngest(.*)',
-  '/api/auth(.*)',
-  '/api/webhooks(.*)',
-  '/api/trpc(.*)',
-]);
+export default clerkMiddleware(async (auth, req) => {
+  if (!isProtectedRoute(req)) return;
 
-export default clerkMiddleware(async(auth,req)=>{
-  console.log("MIDDLEWARE HIT:", req.nextUrl.pathname);
-  if (allowedRoutes(req)) {
-    return NextResponse.next();
-  }
-  const {userId} = await auth();
-  if(!userId && isProtectedRoute(req)) {
-     const {redirectToSignIn} = await auth();
-     return redirectToSignIn();
+  const session = await auth();
+
+  if (!session.userId) {
+    return session.redirectToSignIn(); // ✅ single auth() call
   }
 });
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
 };
